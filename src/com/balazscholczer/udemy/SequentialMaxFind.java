b7e9895ea05ede7396a4c18f0e83a4ecfa600cc9
gabriel7mendes <andrewsseven@gmail.com>
2021-02-18 10:37:29 -0300

    first commit

---
+++ src/com/balazscholczer/udemy/SequentialMaxFind.java
@@ -0,0 +1,16 @@
+package com.balazscholczer.udemy;
+
+public class SequentialMaxFind {
+	
+	public long sequentialMaxFinding(long[] nums, int highIndex) {
+
+		long max = nums[0];
+
+		for (int i = 0; i < highIndex; ++i)
+			if (nums[i] > max)
+				max = nums[i];
+
+		return max;
+	}
+
+}
